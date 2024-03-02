f5c688acef35f71e8aab229bed4718fe1ad8cbbe
nbproduction433 <85150514+nbproduction433@users.noreply.github.com>
2024-03-02 13:07:37 +0530

    d

---
+++ README.md
@@ -0,0 +1,20 @@
+# Automatic Changing Status
+Automatically change your Discord status with this program!
+
+## Usage
+- Download the repo or import it.
+- Go to `main.py` and then line 46.
+- Change the `online` status to whatever you like. (Eg. dnd, idle, online)
+```        
+Run(discord, "online") # Change circle status | dnd, idle, online
+```
+- Go to `messages.txt` and put all the messages you want your status to change to.
+- Enjoy!
+------
+## Pros
+- No BetterDiscord needed.
+- You can use this on the Discord website.
+- Deployable to [Repl.it](https://repl.it).
+------
+## Credits
+- Made by Omit Codez
---
+++ main.py
@@ -0,0 +1,54 @@
+# Made by Omit Codez
+# Leave a star on this repo and follow!
+# Thanks <3
+import keep_alive
+import sys, os, time, requests, random
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+keep_alive.keep_alive()
+class Discord:
+    
+    def __init__(self, token):
+        self.token = token
+        self.headers = {
+            "Authorization": token,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
+            "Content-Type": "application/json",
+            "Accept": "*/*"
+        }
+
+    def ChangeStatus(self, status, message):
+        jsonData = {
+            "status": status,
+            "custom_status": {
+                "text": message,
+        }}
+        r = requests.patch("https://discord.com/api/v8/users/@me/settings", headers=self.headers, json=jsonData)
+        return r.status_code
+
+def Run(discord, status,):
+    discord = discord
+    message = random.choice(list(open('messages.txt')))
+    message = message.replace('\r', '').replace('\n', '')
+    status_code = discord.ChangeStatus(status, message)
+    if status_code == 200:
+        print("|———————————————————————————————————|")
+        print("  Changed your status! ")
+        print(f"  Status: {message}")
+        print("|———————————————————————————————————|")
+    else:
+        print("An error occured. Try again?")
+
+def Main():
+    TOKEN = os.environ.get("TOKEN")
+    discord = Discord(TOKEN)
+    while True:
+        Run(discord, "online") # Change circle status | dnd, idle, online
+        time.sleep(5)
+
+if __name__ == "__main__":
+    Main()
+else:
+    filename = os.path.basename(sys.argv[0])
+    print(f"{filename} is being imported into another module.")
+  
